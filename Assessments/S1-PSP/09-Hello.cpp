0512b188e41cd1b1eb5e950f4496d83d2e7ad3e9
carlosm170895 <carlosm170895@hotmail.com>
2025-11-14 11:43:24 +0000

    Ninth exercice DONE

--- Assessments/S1-PSP/09-Hello.cpp
+++ Assessments/S1-PSP/09-Hello.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   // Lets us use cout to print messages
+using namespace std;  // So we don’t need to type std:: every time
 
-void hello() {
-  cout << "Hello" << endl;
+void hello() {        // A small function that prints a message
+  cout << "Hello" << endl;  // This shows the word "Hello" on the screen
 }
 
-int main() {
+int main() {          // The main part of the program
 
-  hello();
+  hello();            // Calling the hello function so it actually runs
 
-  return 0;
+  return 0;           // End of the program
 }
- 
+
