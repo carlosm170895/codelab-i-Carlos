71ed4f8faf87965909d66162069083a30826e5ec
carlosm170895 <carlosm170895@hotmail.com>
2025-11-14 11:42:28 +0000

    Seventh exercice DONE
    Added comments to clarify the purpose of each counting loop.

--- Assessments/S1-PSP/07-SomeCounting.cpp
+++ Assessments/S1-PSP/07-SomeCounting.cpp
@@ -1,36 +1,41 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   // Lets us print things to the screen
+using namespace std;  // So we don't have to type std:: every time
 
-int main() {
+int main() {          // Program starts here
+
+    // Count from 0 up to 50
     for (int i = 0; i <= 50; i++) {
         cout << i << " ";
     }
-    cout << endl;
+    cout << endl;  // Move to the next line
 
-    
+
+    // Count backwards from 50 down to 0
     for (int i = 50; i >= 0; i--) {
         cout << i << " ";
     }
     cout << endl;
 
-    
+
+    // Count from 30 to 50
     for (int i = 30; i <= 50; i++) {
         cout << i << " ";
     }
     cout << endl;
 
-    
+
+    // Count from 50 down to 10, but skipping by 2 each time
     for (int i = 50; i >= 10; i -= 2) {
         cout << i << " ";
     }
     cout << endl;
 
 
+    // Count from 100 to 200, jumping by 5
     for (int i = 100; i <= 200; i += 5) {
         cout << i << " ";
     }
     cout << endl;
 
-    return 0;
+    return 0;  // End of program
 }
- 
