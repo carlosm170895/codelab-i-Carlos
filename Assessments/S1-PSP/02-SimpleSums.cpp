ce7183339caa68c9676248e933d159aebde79c6f
carlosm170895 <carlosm170895@hotmail.com>
2025-11-14 11:36:08 +0000

    Second exercice DONE
    Added comments to explain code functionality.

--- Assessments/S1-PSP/02-SimpleSums.cpp
+++ Assessments/S1-PSP/02-SimpleSums.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   // Allows use of input/output functions like cout
+using namespace std;  // Enables direct use of standard library names
 
-int main() {
-    int a = 8;
-    int b = 10;
-    int sum = a+b;
-    cout << sum << endl;
+int main() {          // Program entry point
+    int a = 8;        // Declare integer variable a and set it to 8
+    int b = 10;       // Declare integer variable b and set it to 10
+    int sum = a + b;  // Compute the sum of a and b, store in sum
+    cout << sum << endl; // Output the sum to the console
 }
- 
+
