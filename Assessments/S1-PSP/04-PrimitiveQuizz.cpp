458338131cb83af69f971b5dae99ce542b63b08e
carlosm170895 <carlosm170895@hotmail.com>
2025-11-14 11:40:42 +0000

    Fourth exercice DONE

--- Assessments/S1-PSP/04-PrimitiveQuizz.cpp
+++ Assessments/S1-PSP/04-PrimitiveQuizz.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   // Lets us print things and read input
+using namespace std;  // Makes it easier so we don't have to type std:: everywhere
 
-int main() {
-    string answer;
-    int score = 0;
+int main() {          // This is where the program begins
+    string answer;    // A variable to store what the user types as an answer
+    int score = 0;    // This will count how many questions the user gets right
 
-    cout << "What is the capital of France? ";
-    cin >> answer;
-    if (answer == "Paris" || answer == "paris") {
-        cout << "Correct!" << endl;
-        score++;
+    cout << "What is the capital of France? ";  // Ask the first question
+    cin >> answer;                              // Read the user's answer
+    if (answer == "Paris" || answer == "paris") { // Check if correct
+        cout << "Correct!" << endl;             // Tell user they got it right
+        score++;                                // Add 1 point to score
     } else {
-        cout << "Wrong! The correct answer is Paris." << endl;
+        cout << "Wrong! The correct answer is Paris." << endl; // If wrong, tell them
     }
 
-    cout << "What is the capital of Germany? ";
+    cout << "What is the capital of Germany? ";  // Ask second question
     cin >> answer;
     if (answer == "Berlin" || answer == "berlin") {
         cout << "Correct!" << endl;
@@ -23,7 +23,7 @@ int main() {
         cout << "Wrong! The correct answer is Berlin." << endl;
     }
 
-    cout << "What is the capital of Italy? ";
+    cout << "What is the capital of Italy? ";    // Third question
     cin >> answer;
     if (answer == "Rome" || answer == "rome") {
         cout << "Correct!" << endl;
@@ -32,7 +32,7 @@ int main() {
         cout << "Wrong! The correct answer is Rome." << endl;
     }
 
-    cout << "What is the capital of Spain? ";
+    cout << "What is the capital of Spain? ";    // Fourth question
     cin >> answer;
     if (answer == "Madrid" || answer == "madrid") {
         cout << "Correct!" << endl;
@@ -41,7 +41,7 @@ int main() {
         cout << "Wrong! The correct answer is Madrid." << endl;
     }
 
-    cout << "What is the capital of Greece? ";
+    cout << "What is the capital of Greece? ";   // Fifth question
     cin >> answer;
     if (answer == "Athens" || answer == "athens") {
         cout << "Correct!" << endl;
@@ -50,8 +50,7 @@ int main() {
         cout << "Wrong! The correct answer is Athens." << endl;
     }
 
-    cout << "You got " << score << " out of 5 correct!" << endl;
+    cout << "You got " << score << " out of 5 correct!" << endl; // Show final score
 
-    return 0;
+    return 0;  // Program ends
 }
- 
