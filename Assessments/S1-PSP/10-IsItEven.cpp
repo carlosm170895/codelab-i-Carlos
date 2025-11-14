7e53959b6821ed6aed773bdfc75e4a26aba295c1
carlosm170895 <carlosm170895@hotmail.com>
2025-11-14 11:43:49 +0000

    Tenth exercice DONE

--- Assessments/S1-PSP/10-IsItEven.cpp
+++ Assessments/S1-PSP/10-IsItEven.cpp
@@ -1,23 +1,24 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   // Lets us use cout and cin
+using namespace std;  // So we don’t have to write std:: all the time
 
+// This function checks if a number is even or odd
 string checkEven(int num) {
-    if (num % 2 == 0) {
-        return "The provided number is even";
-    } else {
+    if (num % 2 == 0) {                 // If the number divides by 2 with no leftover
+        return "The provided number is even";  
+    } else {                            // Otherwise it's odd
         return "The provided number is odd";
     }
 }
 
-int main() {
-    int number;
+int main() {          // The program starts here
+    int number;       // This will store the number the user types
 
-    cout << "Enter a number: ";
-    cin >> number;
+    cout << "Enter a number: ";  // Ask the user for a number
+    cin >> number;               // Save the number
 
-    string message = checkEven(number);
-    cout << message << endl;
+    string message = checkEven(number);  // Call the function and get the message
+    cout << message << endl;             // Print the result
 
-    return 0;
+    return 0;   // End of program
 }
- 
+
